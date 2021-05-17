930f7cd1d5d4f5896fb907565084d4a4e2cbd92d
Kobi Toshimitsu (therealtboy) <tboy@thenoob.com>
2021-05-16 22:47:08 -0400

    Added Taxation functionality to coin

--- contracts/mock/KaikenToken.sol
+++ contracts/mock/KaikenToken.sol
@@ -8,28 +8,87 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract KaikenToken is ERC20 {
     using SafeMath for uint;
 
+    address owner;
+    address private reserve = 0x3FEE83b4a47D4D6425319A09b91C0559DDF9E31C; // My Sola ETH Account
+
+    uint transferMode;
+    uint[] startingTaxes = [
+        25,
+        30,
+        35,
+        40,
+        45
+    ];
+
+     uint[] thresholds = [
+        10,
+        20,
+        30,
+        40,
+        50
+    ];
+
+    uint taxReductionStep = 1;
+    uint maxTaxReductions = 24;
+    uint taxReductionCadence = 31; 
+
+    //structs 
+    struct TaxRecord {
+        uint timestamp;
+        uint taxReductions;
+    }
+
+    // constants 
     uint private constant BPS = 100;
-    address _kaikenReserveAddr = 0x3FEE83b4a47D4D6425319A09b91C0559DDF9E31C; // My Sola ETH Account
+    uint private constant TOTAL_SUPPLY = 100000000000;
+    uint private constant TRANSFER = 0;
+    uint private constant TRANSFER_FROM = 1;
+
+    // mappings
+    mapping(address => bool) exempts;
+    mapping(address => TaxRecord) genesis;
+    mapping(address => TaxRecord) sandboxGenesis;
+
+    //modifiers
+    modifier onlyOwner {
+        require(msg.sender == owner, 'Only the owner can invoke this call.');
+        _;
+    }
+    // events
+    event AddedExempt(address exempted);
+    event RemovedExempt(address exempted);
+    event UpdatedExempt(address exempted, bool isValid);
+    event UpdatedReserve(address reserve);
+    event UpdatedTaxReductionCadence(uint cadence);
+    event TaxReduced(
+        address accountLiable, 
+        uint now
+    );
+    event TaxRecordSet(address _addr, uint timestamp, uint taxReductions);
+    event UpdatedStartingTaxes(uint[] startingTaxes);
+    event UpdatedTaxReductionStep(uint taxReductionStep);
+    event UpdatedMaxTaxReductions(uint maxTaxReductions);
+    event UpdatedThresholds(uint[] thresholds);
+    event GotTax(address msgSender, uint sentAmount, uint balanceOfSenderOrFrom, uint percentageTransferred , uint taxPercentage);
+
+    // sandbox events
+    event SandboxTaxRecordSet(address _addr, uint timestamp, uint taxReductions);
 
     constructor(
         string memory _name,
         string memory _symbol
     ) public ERC20(_name, _symbol) {
-        _mint(msg.sender, 100000000000 * (10 ** uint256(decimals())));
+        owner = msg.sender;
+        _mint(owner, TOTAL_SUPPLY * (10 ** uint256(decimals())));
     }
 
-    event GotTax(address msgSender, uint sentAmount, uint balanceOfSender, uint percentageTransferred , uint taxPercentage);
-
-    function getReserveAddress() public view returns(address) {
-        return _kaikenReserveAddr;
-    }
-    function getBalance(address addr) public view returns(uint256) {
-        return balanceOf(addr);
-    }
-
-    function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        (, uint taxAmount) = _getReceivedAmount(msg.sender, spender, amount);
-        return super.approve(spender, amount.add(taxAmount));
+    // Overrides
+    function transfer(
+        address to,
+        uint amount
+    ) public virtual override returns(bool){
+        transferMode = TRANSFER;
+        return _internalTransfer(msg.sender, to, amount);
     }
 
     function transferFrom(
@@ -37,12 +96,157 @@ contract KaikenToken is ERC20 {
         address to,
         uint amount
     ) public virtual override returns (bool success) {
-        return _transferFrom(from, to, amount);
+        transferMode = TRANSFER_FROM;
+        return _internalTransfer(from, to, amount);
+    }
+
+    // Reads
+    function getStartingTaxes() public view returns(uint[] memory) {
+        return startingTaxes;
+    }
+
+    function getThresholds() public view returns(uint[] memory){
+        return thresholds;
+    }
+
+    function getExempt(address _addr) public view returns(bool){
+        return exempts[_addr];
+    }
+
+    function getReserve() public view returns(address) {
+        return reserve;
+    }
+
+    function getTaxReductionCadence() public view returns(uint) {
+        return taxReductionCadence;
+    }
+
+    function getMaxTaxReductions() public view returns(uint) {
+        return maxTaxReductions;
+    }
+
+    function getTaxReductionsFromGenesis(address _addr) public view returns(uint) {
+        return genesis[_addr].taxReductions;
+    }
+
+    function isDueForTaxReduction(address accountLiable) public view returns(bool) {
+        return (
+            genesis[accountLiable].taxReductions <= maxTaxReductions &&
+            (block.timestamp <= genesis[accountLiable].timestamp.add(taxReductionCadence * 1 days))
+        );
+    }
+
+    // Writes
+    function updateStartingTaxes(uint[] memory _startingTaxes) public onlyOwner {
+        startingTaxes = _startingTaxes;
+        emit UpdatedStartingTaxes(startingTaxes);
+    }
+
+    function updateTaxReductionStep(uint _step) public onlyOwner {
+        taxReductionStep = _step;
+        emit UpdatedTaxReductionStep(taxReductionStep);
+    }
+
+    function updateMaxTaxReductions(uint _maxTaxReductions) public onlyOwner {
+        maxTaxReductions = _maxTaxReductions;
+        emit UpdatedMaxTaxReductions(maxTaxReductions);
+    }
+
+    function updateThresholds(uint[] memory _thresholds) public onlyOwner {
+        thresholds = _thresholds;
+        emit UpdatedThresholds(thresholds);
+    }
+
+    function updateReserve(address _reserve) public onlyOwner {
+        reserve = _reserve;
+        emit UpdatedReserve(reserve);
+    }
+
+    function addExempt(address _exempted) public onlyOwner {
+        require(!exempts[_exempted], 'Exempt address already existent'); 
+            
+        exempts[_exempted] = true;
+        emit AddedExempt(_exempted);
+    }
+
+     function updateExempt(address _exempted, bool isValid) public onlyOwner {
+        require(exempts[_exempted], 'Exempt address is not existent'); 
+
+        exempts[_exempted] = isValid;
+        emit UpdatedExempt(_exempted, isValid);
+    }
+
+    function removeExempt(address _exempted) public onlyOwner {
+        require(exempts[_exempted], 'Exempt address is not existent'); 
+
+        exempts[_exempted] = false;
+        emit RemovedExempt(_exempted);
+    }
+
+    function updateTaxReductionCadence(uint _taxReductionCadence) public onlyOwner {
+        taxReductionCadence = _taxReductionCadence;
+        emit UpdatedTaxReductionCadence(taxReductionCadence);
+    }
+
+    // internal functions
+    function _getTaxPercentage(
+        address _from,
+        uint _sentAmount
+    ) internal returns (uint tax) {
+        uint taxPercentage = 0;
+        uint balanceOfSenderOrFrom = balanceOf(_from);
+
+        require(
+            balanceOfSenderOrFrom > 0 && _sentAmount > 0,
+            'Intangible balance or amount to send'
+        );
+
+        address accountLiable = transferMode == TRANSFER_FROM
+            ? _from
+            : msg.sender; 
+
+        if (exempts[accountLiable]) return taxPercentage;
+
+        uint percentageTransferred = _sentAmount.mul(100).div(balanceOfSenderOrFrom);
+
+        if (percentageTransferred <= thresholds[0]) {
+            taxPercentage = startingTaxes[0];
+        }else if (percentageTransferred <= thresholds[1]) {
+            taxPercentage = startingTaxes[1];
+        }else if (percentageTransferred <= thresholds[2]) {
+            taxPercentage = startingTaxes[2];
+        }else if (percentageTransferred <= thresholds[3]) {
+            taxPercentage = startingTaxes[3];
+        } else {
+            taxPercentage = startingTaxes[4];
+        }
+
+        if (genesis[accountLiable].timestamp == 0) {
+            _setGenesisTaxRecord(accountLiable, 0);
+        }
+
+        bool isDue = isDueForTaxReduction(accountLiable);
+
+        if (isDue) {
+            genesis[accountLiable] = TaxRecord({ 
+                timestamp: block.timestamp,
+                taxReductions: genesis[accountLiable].taxReductions++
+            });
+            _setGenesisTaxRecord(accountLiable, genesis[accountLiable].taxReductions++);
+            emit TaxReduced(
+                accountLiable,
+                block.timestamp
+            );
+        }
+        
+        taxPercentage = taxPercentage.sub(genesis[accountLiable].taxReductions.mul(taxReductionStep));
+        
+        emit GotTax(_from, _sentAmount, balanceOfSenderOrFrom, percentageTransferred, taxPercentage);
+        return taxPercentage;
     }
 
     function _getReceivedAmount(
         address _from,
-        address, /*_to*/
         uint _sentAmount
     ) internal returns (uint receivedAmount, uint taxAmount) {
         uint taxPercentage = _getTaxPercentage(_from, _sentAmount);
@@ -50,52 +254,58 @@ contract KaikenToken is ERC20 {
         taxAmount = _sentAmount.sub(receivedAmount);
     }
 
-    function _transferFrom(
-        address _from,
+    function _setGenesisTaxRecord(
+        address _addr, 
+        uint _taxReductions
+        ) internal {
+        uint timestamp = block.timestamp;
+        genesis[_addr] = TaxRecord({ 
+            timestamp: timestamp,
+            taxReductions: _taxReductions
+        });
+        emit TaxRecordSet(_addr, timestamp, _taxReductions);
+    }
+
+    function _internalTransfer(
+        address _from, // `msg.sender` || `from`
         address _to,
         uint _amount
     ) internal returns (bool success){
-        (, uint taxAmount) = _getReceivedAmount(_from, _to, _amount);
-        bool transferred = super.transferFrom(_from, _kaikenReserveAddr, taxAmount);
+        (, uint taxAmount) = _getReceivedAmount(_from, _amount);
+        require(
+            balanceOf(_from) > _amount.add(taxAmount),
+            'Cannot afford to pay tax'
+        ); 
         
-        if(transferred == true) {
-            return super.transferFrom(
-                _from, 
-                _to, 
-                _amount
-            );
+        if(taxAmount > 0) {
+            _burn(_from, taxAmount);
+            _mint(reserve, taxAmount);
         }
+        
+        transferMode == TRANSFER 
+            ? super.transfer(_to, _amount) 
+            : super.transferFrom(_from, _to, _amount);
+
+        return true;
     }
 
-    function _getTaxPercentage(
-        address _from,
-        uint _sentAmount
-    ) public returns (uint tax) {
-        uint taxPercentage;
-        uint balanceOfSender = balanceOf(_from);
-
-        require(
-            balanceOfSender > 0 && _sentAmount > 0,
-            'Intangible balance or amount to send'
+    // Sandbox functions
+    function sandboxIsDueForTaxReduction(address accountLiable) public view returns(bool) {
+        return (
+            sandboxGenesis[accountLiable].taxReductions <= maxTaxReductions &&
+            (block.timestamp <= sandboxGenesis[accountLiable].timestamp.add(taxReductionCadence * 1 days))
         );
-
-        uint percentageTransferred = _sentAmount.mul(100).div(balanceOfSender);
-
-        if (percentageTransferred <= 5) {
-            taxPercentage = 8;
-        }else if (percentageTransferred <= 10) {
-            taxPercentage = 12;
-        }else if (percentageTransferred <= 20) {
-            taxPercentage = 20;
-        }else if (percentageTransferred <= 30) {
-            taxPercentage = 30;
-        }else if (percentageTransferred <= 50) {
-            taxPercentage = 40;
-        } else {
-            taxPercentage = 90;
-        }
-
-        emit GotTax(_from, _sentAmount, balanceOfSender, percentageTransferred, taxPercentage);
-        return taxPercentage;
+    }
+    
+    function sandboxSetGenesisTaxRecord(
+        address _addr, 
+        uint _taxReductions
+        ) public {
+        uint timestamp = block.timestamp;
+        sandboxGenesis[_addr] = TaxRecord({ 
+            timestamp: timestamp,
+            taxReductions: _taxReductions
+        });
+        emit SandboxTaxRecordSet(_addr, timestamp, _taxReductions);
     }
 }
--- test/Kaikentoken.test.js
+++ test/Kaikentoken.test.js
@@ -38,119 +38,134 @@ contract('KaikenToken', async function ([creator, other]) {
     expect(await this.token.balanceOf(creator)).to.be.bignumber.equal(TOTAL_SUPPLY)
   })
 
-  it('should return the token balance of an address', async function () {
-    expect(await this.token.getBalance(creator)).to.be.bignumber.equal(TOTAL_SUPPLY)
+  it('should update return the reserve account', async function () {
+    let tx = await this.token.updateReserve(other)
+    expect(await this.token.getReserve()).to.be.equal(other)
   })
 
-  it('should fail in an attempt to return the taxPercentage since amount is zero', async function () {
-    await truffleAssert.fails(
-      this.token._getTaxPercentage(creator, 0),
-      truffleAssert.ErrorType.REVERT
-    )
+  it('should update and return the starting taxes', async function () {
+    let startingTaxes = [1, 2, 3, 5, 10]
+    let tx = await this.token.updateStartingTaxes(startingTaxes)
+    await truffleAssert.eventEmitted(tx, 'UpdatedStartingTaxes', (ev) => {
+      let taxes = ev.startingTaxes.map(tax => parseInt(tax.toString()))
+      let reducedTaxes = taxes.reduce((acc, currentValue) => acc * currentValue)
+      let reducedStartingTaxes = startingTaxes.reduce((acc, currentValue) => acc * currentValue)
+      return reducedTaxes == reducedStartingTaxes
+    })
   })
 
-  it('should not fail in an attempt to return the taxPercentage since amount is tangible', async function () {
-    await truffleAssert.passes(
-      this.token._getTaxPercentage(creator, 10000)
-    )
+  it('should update and return the reduction step', async function () {
+    let tx = await this.token.updateTaxReductionStep(2)
+    await truffleAssert.eventEmitted(tx, 'UpdatedTaxReductionStep', (ev) => {
+      return ev.taxReductionStep == 2
+    })
   })
 
-  it('should emit an event with percentageTransferred and taxPercentage', async function () {
-    let sentAmount = 10000
-    let tx = await this.token._getTaxPercentage(creator, sentAmount)
+  it('should update and return the max tax reductions', async function () {
+    let tx = await this.token.updateMaxTaxReductions(10)
+    await truffleAssert.eventEmitted(tx, 'UpdatedMaxTaxReductions', (ev) => {
+      return ev.maxTaxReductions == 10
+    })
+  })
 
-    await truffleAssert.eventEmitted(tx, 'GotTax', (ev) => {
+  it('should add an exempt', async function () {
+    let tx = await this.token.addExempt(other)
+    await truffleAssert.eventEmitted(tx, 'AddedExempt', (ev) => {
+      return ev.exempted == other
+    })
+  })
+
+  it('should remove an exempt', async function () {
+    await this.token.addExempt(other)
+    let tx = await this.token.removeExempt(other)
+    await truffleAssert.eventEmitted(tx, 'RemovedExempt', (ev) => {
+      return ev.exempted == other
+    })
+  })
+
+  it('should update an exempt', async function () {
+    await this.token.addExempt(other)
+    let tx = await this.token.updateExempt(other, false)
+    await truffleAssert.eventEmitted(tx, 'UpdatedExempt', (ev) => {
       return (
-        ev.taxPercentage == 8 &&
-        ev.percentageTransferred < 5
+        ev.exempted == other &&
+        !ev.isValid
       )
     })
   })
 
-  it('creator balance should equal balance from emitted event before transfer', async function () {
-    let sentAmount = 100000000
-    let creatorBalance = await this.token.getBalance(creator)
-
-    let tx = await this.token._getTaxPercentage(
-      creator,
-      sentAmount
-    )
-
-    await truffleAssert.eventEmitted(tx, 'GotTax', (ev) => {
-      return ev.balanceOfSender.toString() == creatorBalance.toString()
-    })
-  })
-
-  it('should have the correct tax applied to an amount potentially sent ', async function () {
-    let sentAmount = '50000000000000000000000000000'
-    let creatorBalance = await this.token.getBalance(creator)
-    let tx = await this.token._getTaxPercentage(creator, sentAmount)
-
-    await truffleAssert.eventEmitted(tx, 'GotTax', (ev) => {
+  it('should set a genesis tax record', async function () {
+    let tx = await this.token.sandboxSetGenesisTaxRecord(other, 2)
+    await truffleAssert.eventEmitted(tx, 'SandboxTaxRecordSet', (ev) => {
       return (
-        ev.taxPercentage == 40 &&
-        ev.percentageTransferred == (sentAmount * 100) / creatorBalance
+        ev._addr == other &&
+        ev.timestamp <= Date.now() &&
+        ev.taxReductions == 2
       )
     })
   })
 
-  it('should apply a 90% tax to an amount that is potentially sent ', async function () {
-    let sentAmount = '80000000000000000000000000000'
-    let tx = await this.token._getTaxPercentage(creator, sentAmount)
+  it('should emit a `GotTax` event with percentageTransferred and taxPercentage', async function () {
+    let sentAmount = '500000000000000000000000000'
+    let tx = await this.token.transfer(other, sentAmount)
 
     await truffleAssert.eventEmitted(tx, 'GotTax', (ev) => {
       return (
-        ev.taxPercentage == 90 &&
-        ev.percentageTransferred > 50
+        ev.taxPercentage < 50 &&
+        ev.percentageTransferred < 10
       )
     })
   })
 
   it('should transfer tokens successfully', async function () {
-    let initialBalCreator = await this.token.getBalance(creator)
-    let initialBalOther = await this.token.getBalance(other)
+    let initialBalCreator = await this.token.balanceOf(creator)
+    let initialBalOther = await this.token.balanceOf(other)
 
-    let obj = await this.token.transfer(other, 1000)
+    let { receipt } = await this.token.transfer(other, 100 * Math.pow(10, 8))
 
-    expect(obj).to.have.any.keys('tx', 'receipts')
-    expect(obj.tx).to.have.string('0x')
-    expect(obj.receipt.transactionHash).to.have.string('0x')
-    expect(await this.token.getBalance(creator)).to.be.bignumber.lessThan(initialBalCreator)
-    expect(await this.token.getBalance(other)).to.be.bignumber.greaterThan(initialBalOther)
+    expect(receipt.transactionHash).to.have.string('0x')
+    expect(await this.token.balanceOf(creator)).to.be.bignumber.lessThan(initialBalCreator)
+    expect(await this.token.balanceOf(other)).to.be.bignumber.greaterThan(initialBalOther)
   })
 
-  it('should not credit the KAIKEN reserve after transfer is invoked', async function () {
-    let reserveAddr = await this.token.getReserveAddress()
-    let initialBalOther = await this.token.getBalance(other)
-    let initialBalCreator = await this.token.getBalance(creator)
-    let initialBalReserveAddr = await this.token.getBalance(reserveAddr)
+  it('should credit the kR after transfer is invoked', async function () {
+    let reserveAddr = await this.token.getReserve()
+    let initialBalOther = await this.token.balanceOf(other)
+    let initialBalCreator = await this.token.balanceOf(creator)
+    let initialBalReserveAddr = await this.token.balanceOf(reserveAddr)
 
-    let obj = await this.token.transfer(other, 1000)
+    let { receipt } = await this.token.transfer(other, 100 * Math.pow(10, 8))
 
-    expect(obj).to.have.any.keys('tx', 'receipts')
-    expect(obj.receipt.transactionHash).to.have.string('0x')
-    expect(await this.token.getBalance(other)).to.be.bignumber.greaterThan(initialBalOther)
-    expect(await this.token.getBalance(creator)).to.be.bignumber.lessThan(initialBalCreator)
-    expect(await this.token.getBalance(reserveAddr)).to.be.bignumber.equal(initialBalReserveAddr)
+    expect(receipt.transactionHash).to.have.string('0x')
+    expect(await this.token.balanceOf(other)).to.be.bignumber.greaterThan(initialBalOther)
+    expect(await this.token.balanceOf(creator)).to.be.bignumber.lessThan(initialBalCreator)
+    expect(await this.token.balanceOf(reserveAddr)).to.be.bignumber.greaterThan(initialBalReserveAddr)
   })
 
   it('should successfully approve the spender `other`', async function () {
-    let { tx, receipt } = await this.token.approve(other, 1000)
+    let { tx, receipt } = await this.token.approve(other, 100000000)
     expect(tx).to.have.string('0x')
     expect(receipt.transactionHash).to.have.string('0x')
   })
 
   it('should grant authority to account `other` to spend from account `creator`', async function () {
+    let { receipt: transferReceipt } = await this.token.transfer(other, 100000 * Math.pow(10, 8))
+
     let allowedAmount = 100000
-    let to = '0x4cbbA18De7DEEBbb370B360932b64767626CEBfD'
-    let reserve = await this.token.getReserveAddress()
+    let to = '0x73Fc15691e3F3f5322271bbB97d65D7bc456D1A1'
+    let reserve = await this.token.getReserve()
+    let toBal = await this.token.balanceOf(to)
+    let otherBal = await this.token.balanceOf(other)
+    let creatorBal = await this.token.balanceOf(creator)
+    let reserveBal = await this.token.balanceOf(reserve)
 
-    let toBal = await this.token.getBalance(to)
-    let otherBal = await this.token.getBalance(other)
-    let creatorBal = await this.token.getBalance(creator)
-    let reserveBal = await this.token.getBalance(reserve)
-
-    let { receipt: approvalReceiptOther } = await this.token.approve(other, allowedAmount)
+    let { receipt: approvalReceiptOther } = await this.token.approve(
+      other,
+      allowedAmount,
+      {
+        from: creator
+      }
+    )
 
     let allowedOther = await this.token.allowance(creator, other)
 
@@ -163,12 +178,34 @@ contract('KaikenToken', async function ([creator, other]) {
       }
     )
 
-    expect(allowedOther.toNumber()).to.be.greaterThan(allowedAmount) // `allowedOther` is greater due to the applied tax
+    let settledCreatorBal = await this.token.balanceOf(creator)
+
+    expect(allowedOther.toNumber()).to.be.equal(allowedAmount) // `allowedOther` is greater due to the applied tax
     expect(approvalReceiptOther.transactionHash).to.have.string('0x')
+    expect(transferReceipt.transactionHash).to.have.string('0x')
     expect(transferFromReceipt.transactionHash).to.have.string('0x')
-    expect(await this.token.getBalance(other)).to.be.bignumber.equal(otherBal) // unchanged
-    expect(await this.token.getBalance(creator)).to.be.bignumber.lessThan(creatorBal) // depreciated balance
-    expect(await this.token.getBalance(to)).to.be.bignumber.greaterThan(toBal) // appreciated balance
-    expect(await this.token.getBalance(reserve)).to.be.bignumber.greaterThan(reserveBal) // appreciated balance
+    expect(await this.token.balanceOf(other)).to.be.bignumber.equal(otherBal) // unchanged
+    expect(settledCreatorBal).to.be.bignumber.lessThan(creatorBal) // depreciated balance
+
+    expect(settledCreatorBal).to.be.bignumber.lessThan(creatorBal) // depreciated balance
+
+    expect(await this.token.balanceOf(to)).to.be.bignumber.greaterThan(toBal) // appreciated balance
+    expect(await this.token.balanceOf(reserve)).to.be.bignumber.greaterThan(reserveBal) // appreciated balance due to paid taxes
+  })
+
+  it('should not tax an exempted account ', async function () {
+    await this.token.addExempt(creator)
+
+    let reserve = await this.token.getReserve()
+    let initialBalCreator = await this.token.balanceOf(creator)
+    let initialBalOther = await this.token.balanceOf(other)
+    let initialBalReserve = await this.token.balanceOf(reserve)
+
+    let { receipt } = await this.token.transfer(other, 100 * Math.pow(10, 8))
+
+    expect(receipt.transactionHash).to.have.string('0x')
+    expect(await this.token.balanceOf(creator)).to.be.bignumber.lessThan(initialBalCreator)
+    expect(await this.token.balanceOf(other)).to.be.bignumber.greaterThan(initialBalOther)
+    expect(await this.token.balanceOf(reserve)).to.be.bignumber.equal(initialBalReserve)
   })
 })
